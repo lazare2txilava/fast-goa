d2704c8f9895abd9af09bb39062a431498dc8cb6
lazare2txilava <1lazotxilo09@gmail.com>
2024-10-19 20:11:46 +0400

    commit

---
+++ level24/homework/homework 1.py
@@ -0,0 +1,64 @@
+
+my_list = [1, 2, 3]
+
+
+my_list.append(4)  
+
+
+
+my_list.remove(2)  
+
+
+my_list[0] = 10
+
+
+
+length = len(my_list)  
+
+
+
+exists = 3 in my_list  #
+
+
+
+
+
+
+my_tuple = (1, 2, 3)
+
+
+
+first_element = my_tuple[0]  
+
+
+
+length = len(my_tuple)  
+
+
+exists = 2 in my_tuple 
+
+
+new_tuple = my_tuple + (4, 5)  
+
+
+
+
+
+
+my_set = {1, 2, 3}
+
+
+
+my_set.add(4)  
+
+
+
+my_set.remove(2)  
+
+
+
+another_set = {3, 4, 5}
+intersection = my_set.intersection(another_set)  
+
+
+length = len(my_set)  
